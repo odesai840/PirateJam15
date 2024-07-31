1ba81bcfe4d2da5a3720ee8aaeb58953e3e8f295
Tuckere13 <tuckere13@gmail.com>
2024-07-31 07:00:25 -0400

    Bridge

--- Assets/Teleporter.cs
+++ Assets/Teleporter.cs
@@ -6,32 +6,17 @@ public class Teleporter : MonoBehaviour
 {
 
     public GameObject player;
-    Rigidbody rb;
     private PlayerControls playerControls;
     public GameObject teleportExit;
 
-    private void Update()
-    {
-        //Debug.Log(player.transform.position);
-        //Debug.Log(teleportExit.transform.position);
-    }
-    private void Start()
-    {
-        //rb = player.GetComponent<Rigidbody>();
-        //player
-    }
-    private void Awake()
-    {
-        //playerControls = new PlayerControls();
-
-    }
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Hit 1");
         if (collision.CompareTag("Player"))
         {
             Debug.Log("Hit 2");
-            rb.transform.position = new Vector3(-5, 188, 0);
+            player.transform.position = new Vector3(-5, 188, 0);
         }
     }
 }
